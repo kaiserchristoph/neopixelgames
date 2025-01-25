5af2a0eb309d7d2e221b404136a85c1370bc56da
Christoph Kaiser <chrikaiser@gmail.com>
2025-01-26 00:43:00 +0100

    fix set_pixel(), add get_pixel_color(), add connect_wifi(), add bluetooth_scan()

--- main.py
+++ main.py
@@ -2,25 +2,58 @@ import time
 from neopixel import NeoPixel
 from machine import Pin
 from random import randint
+import wifi_config
+
+async def bluetooth_scan():
+    import aioble
+    async with aioble.scan(duration_ms=5000, interval_us=30000, window_us=30000, active=True) as scanner:
+        async for result in scanner:
+            print(result, result.name(), result.rssi, result.services())
+
+
+def connect_wifi():
+    import network
+    wlan = network.WLAN(network.WLAN.IF_STA)
+    wlan.active(False)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(wifi_config.ssid, wifi_config.password)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ipconfig('addr4'))
 
 class Snake:
     def __init__(self):
         self.np = NeoPixel(Pin(32), 256)
-        self.snake = [[0, 0], [1, 0], [2, 0]]
+        self.snake = [[0, 7], [1, 7], [2, 7]]
         self.color = (255, 212, 59)
         self.vx = 1
         self.vy = 0
         self.add_food()
 
     def set_pixel(self, x, y, color):
-        self.np[x + y * 16] = color
+        if y % 2 == 0:
+            index = x + y * 32
+        else:
+            index = (y + 1) * 32 - 1 - x
+        self.np[index] = color
+
+    def get_pixel_color(self, x, y):
+        if y % 2 == 0:
+            index = x + y * 32
+        else:
+            index = (y + 1) * 32 - 1 - x
+        return self.np[index]
 
     def add_food(self):
-        x = randint(0, 64)
-        y = randint(0, 8)
+        x = randint(0, 31)
+        y = randint(0, 7)
+        x = 6
+        y = 7
         while [x, y] in self.snake:
-            x = randint(0, 64)
-            y = randint(0, 8)
+            x = randint(0, 31)
+            y = randint(0, 7)
         self.set_pixel(x, y, (255, 0, 0))
 
     def move(self):
@@ -30,7 +63,7 @@ class Snake:
         if new_head[0] < 0 or new_head[0] >= 32 or new_head[1] < 0 or new_head[1] >= 8:
             return False
         self.snake.append(new_head)
-        if self.np[new_head[0] + new_head[1] * 16] == (255, 0, 0):
+        if self.get_pixel_color(new_head[0], new_head[1]) == (255, 0, 0):
             self.add_food()
         else:
             tail = self.snake.pop(0)
@@ -48,5 +81,7 @@ class Snake:
 
 
 noise = Pin(15, Pin.IN, Pin.PULL_DOWN)    
+connect_wifi()
+bluetooth_scan()
 snakegame = Snake()
 snakegame.run()
